4f82eb63bb25e429c56093e29497176382f4a1b5
rwndy <riwandy10@gmail.com>
2020-10-22 17:20:41 +0700

    constanta pada Go

---
+++ latihan-6/const.md
@@ -0,0 +1,4 @@
+## Membuat Constanta pada Go
+
+const adalah sebuah variabel yang nilainya tidak bisa diubah jika sudah diisi sebelumnya.
+const juga bisa di deklrasikan multiple seperti var
---
+++ latihan-6/main.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	const width = 100
+	const height = 20
+
+	const (
+		firstName = "Udin"
+		lastName  = "Sedunia"
+	)
+
+	println(width)
+	println(height)
+	println(firstName)
+	println(lastName)
+}
